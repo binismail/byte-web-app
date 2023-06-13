d2627aa4e659417709a1bb7f4831a15433714b7f
Bravo Stack <chukwuebukaonuh23@gmail.com>
2023-06-13 14:38:26 +0100

    fix: remove .ts from nextjs

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  // pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  pageExtensions: ['tsx', 'jsx', 'js'],
 };
 
 module.exports = nextConfig;
