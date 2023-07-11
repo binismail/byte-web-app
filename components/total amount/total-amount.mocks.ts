0493d9351b6d45662a4c02752d00a22d6b51509e
Bravo Stack <chukwuebukaonuh23@gmail.com>
2023-07-11 16:34:41 +0100

    fix: git push

--- components/total amount/total-amount.mocks.ts
+++ components/total amount/total-amount.mocks.ts
@@ -2,6 +2,9 @@ import { ITotalAmount } from './total-amount';
 
 const base: ITotalAmount = {
   value: '',
+  subTotal: 0,
+  taxAmount: 0,
+  discountPercentage: 0,
 };
 
 export const mockTotalAmountProps = {
